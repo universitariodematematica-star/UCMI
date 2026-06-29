3b5b86c6de98838414fdae055daa0688c7edc2c9
universitariodematematica-star <universitariodematematica@gmail.com>
2026-06-29 09:14:17 -0500

    Update firebase-config.js

--- componentes/firebase-config.js
+++ componentes/firebase-config.js
@@ -30,21 +30,13 @@ export const CONFIG = {
 // ===============================================
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyA2eMtX0I2u1iKdtHjNisMrqVSlpJbzHNI",
-
     authDomain: "ucmi-13796634.firebaseapp.com",
-
     projectId: "ucmi-13796634",
-
     storageBucket: "ucmi-13796634.firebasestorage.app",
-
     messagingSenderId: "1090719609536",
-
     appId: "1:1090719609536:web:8d7f269d991d8dc3c6b325",
-
     measurementId: "G-3M3DH25722"
-
 };
 
 const app = initializeApp(firebaseConfig);
@@ -54,7 +46,5 @@ const app = initializeApp(firebaseConfig);
 // ===============================================
 
 export { app };
-
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
