3d7d654ac1aa3db00085e3d3cede8eca9aeef254
universitariodematematica-star <universitariodematematica@gmail.com>
2026-07-24 19:04:06 -0500

    Update motor-ejercicios.js

--- motor-ejercicios.js
+++ motor-ejercicios.js
@@ -7,3 +7,92 @@ async function cargarEjercicios(codigo){
 window.UCMIEjercicios = {
     cargarEjercicios
 };
+
+//========================================
+// Generar ejercicios selección simple
+//========================================
+
+let htmlSelSimple = "";
+
+ejerciciosSelSimple.forEach((ejercicio, indice)=>{
+
+
+    let opciones = [
+
+        ejercicio.correcta,
+
+        ejercicio.incorrecta1,
+
+        ejercicio.incorrecta2,
+
+        ejercicio.incorrecta3
+
+    ];
+
+
+    // Mezclar opciones aleatoriamente
+
+    for(let i = opciones.length - 1; i > 0; i--){
+
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [opciones[i], opciones[j]] =
+    [opciones[j], opciones[i]];
+
+}
+
+
+    htmlSelSimple += `
+
+<div class="ejercicio-sel-simple">
+
+<h3>
+${indice + 1}. ${ejercicio.pregunta}
+</h3>
+
+
+<div class="opciones">
+
+${opciones.map((opcion,i)=>`
+
+<label class="opcion-radio">
+
+<input
+type="radio"
+name="pregunta${indice}"
+value="${opcion}">
+
+${String.fromCharCode(97+i)}) ${opcion}
+
+</label>
+
+`).join("")}
+
+</div>
+
+
+<button
+class="verificar"
+onclick="verificarPregunta(
+
+this,
+
+'${ejercicio.correcta}',
+
+'${ejercicio.explicacion}'
+
+)">
+
+Verificar
+
+</button>
+
+
+<div class="resultado"></div>
+
+
+</div>
+
+`;
+
+});
