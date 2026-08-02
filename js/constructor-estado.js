a72f4fe31cc7454b669e3eb4fd3f4b55a857bdde
universitariodematematica-star <universitariodematematica@gmail.com>
2026-08-02 13:35:33 -0500

    Create constructor-estado.js

---
+++ js/constructor-estado.js
@@ -0,0 +1,17 @@
+/*=====================================================
+ESTADO GLOBAL DEL CONSTRUCTOR
+=====================================================*/
+
+let datosPagina = {};
+
+let vocabularioGlobal = [];
+let subtitulosGlobal = [];
+
+let ejerciciosSelSimple = [];
+let ejerciciosCompletar = [];
+let ejerciciosDragDrop = [];
+let ejerciciosOrdenarOracion = [];
+let ejerciciosEmparejarColumnas = [];
+let ejerciciosTraduccion = [];
+
+let configuracionMostrar = {};
