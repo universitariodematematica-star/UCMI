9c888d9a87e96e9780a9f8135d386641a67b6f83
universitariodematematica-star <universitariodematematica@gmail.com>
2026-08-12 23:06:37 -0500

    Update motor-completar-listening.js

--- js/motor-completar-listening.js
+++ js/motor-completar-listening.js
@@ -1,1 +1,35 @@
+/*=====================================================
+    MOTOR - COMPLETAR TEXTO LISTENING
+    UCMI
+=====================================================*/
 
+const UCMIMotorCompletarListening = {
+
+    generar: function(datos){
+
+        if(!datos){
+
+            console.error(
+                "No se recibieron datos para Completar texto listening."
+            );
+
+            return;
+
+        }
+
+        console.log(
+            "Datos Completar texto listening:",
+            datos
+        );
+
+    }
+
+};
+
+
+/*=====================================================
+    EXPONER MOTOR
+=====================================================*/
+
+window.UCMIMotorCompletarListening =
+    UCMIMotorCompletarListening;
