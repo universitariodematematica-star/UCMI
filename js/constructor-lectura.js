f4e3ae1efaa55b5f6028342938ae2dc33c32c35b
universitariodematematica-star <universitariodematematica@gmail.com>
2026-08-12 22:24:31 -0500

    Update constructor-lectura.js

--- js/constructor-lectura.js
+++ js/constructor-lectura.js
@@ -1126,6 +1126,7 @@ function lanzarGeneracion(){
         ejerciciosTranscripcion,
         ejercicioIdentificarImagenes,
         estructurasGlobal,
+        ejercicioCompletarTextoListening,
         configuracionMostrar
     );
 
