456ef3ff604ad2f326346bc2eaee1fd0f5480683
universitariodematematica-star <universitariodematematica@gmail.com>
2026-08-15 23:35:02 -0500

    Update motor-comprension-texto.js

--- js/motor-comprension-texto.js
+++ js/motor-comprension-texto.js
@@ -23,6 +23,35 @@ const UCMIMotorComprensionTexto = {
             configuracion.comprensionTexto;
 
 
+        console.log(
+            "========== MOTOR COMPRENSIÓN DE TEXTO =========="
+        );
+
+        console.log(
+            "CONFIGURACIÓN RECIBIDA:",
+            configuracion
+        );
+
+        console.log(
+            "DATOS RECIBIDOS:",
+            datos
+        );
+
+        console.log(
+            "PÁRRAFOS RECIBIDOS:",
+            datos ? datos.parrafos : null
+        );
+
+        console.log(
+            "PREGUNTAS RECIBIDAS:",
+            datos ? datos.preguntas : null
+        );
+
+        console.log(
+            "===============================================" 
+        );
+
+
         if(!datos){
             console.error(
                 "UCMIMotorComprensionTexto: no se recibieron datos."
