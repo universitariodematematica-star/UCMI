16526458ad29d3f210818bc50f8d97d3d0ef0fbe
universitariodematematica-star <universitariodematematica@gmail.com>
2026-08-29 10:16:55 -0500

    Update reporte-asistencia.js

--- learningbridge/reporte-asistencia.js
+++ learningbridge/reporte-asistencia.js
@@ -1,1 +1,15 @@
+/*
+ * ==================================================
+ * REPORTE DE ASISTENCIA
+ * ==================================================
+ */
 
+document
+    .getElementById("btnDescargarExcel")
+    .addEventListener("click", function() {
+
+        console.log(
+            "MÓDULO REPORTE-ASISTENCIA.JS CONECTADO"
+        );
+
+    });
