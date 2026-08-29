007825ba0d7529db0d5138f1cc8fb33096961488
universitariodematematica-star <universitariodematematica@gmail.com>
2026-08-29 10:54:56 -0500

    Update reporte-asistencia.js

--- learningbridge/reporte-asistencia.js
+++ learningbridge/reporte-asistencia.js
@@ -4,79 +4,19 @@
  * ==================================================
  */
 
-/*
- * ==================================================
- * REPORTE DE ASISTENCIA
- * ==================================================
- */
-
 document
     .getElementById("btnDescargarExcel")
     .addEventListener("click", async function() {
 
-        /*
-         * ----------------------------------------------
-         * CREAR LIBRO
-         * ----------------------------------------------
-         */
-
         const libro = new ExcelJS.Workbook();
 
+        const hoja = libro.addWorksheet("REPORTE");
 
-        /*
-         * ----------------------------------------------
-         * OBTENER GRUPOS
-         * ----------------------------------------------
-         */
-
-        const grupos = [
-            ...new Set(
-                registrosAsistencia
-                    .map(alumno => alumno.grupo)
-                    .filter(grupo => grupo)
-            )
-        ];
-
-
-        console.log(
-            "GRUPOS ENCONTRADOS:",
-            grupos
-        );
-
-
-        /*
-         * ----------------------------------------------
-         * CREAR HOJAS
-         * ----------------------------------------------
-         */
-
-        grupos.forEach(grupo => {
-
-            const hoja = libro.addWorksheet(
-                String(grupo)
-            );
-
-            hoja.getCell("A1").value =
-                `GRUPO: ${grupo}`;
-
-        });
-
-
-        /*
-         * ----------------------------------------------
-         * GENERAR ARCHIVO
-         * ----------------------------------------------
-         */
+        hoja.getCell("A1").value =
+            "PRUEBA DE REPORTE DE ASISTENCIA";
 
         const buffer =
             await libro.xlsx.writeBuffer();
-
-
-        /*
-         * ----------------------------------------------
-         * CREAR DESCARGA
-         * ----------------------------------------------
-         */
 
         const blob = new Blob(
             [buffer],
@@ -89,18 +29,15 @@ document
         const enlace =
             document.createElement("a");
 
-        enlace.style.display = "none";
-
         enlace.href =
             URL.createObjectURL(blob);
 
         enlace.download =
-            "Reporte_Asistencia.xlsx";
+            "Prueba_Reporte.xlsx";
 
         document.body.appendChild(enlace);
 
         enlace.click();
 
         document.body.removeChild(enlace);
-
     });
