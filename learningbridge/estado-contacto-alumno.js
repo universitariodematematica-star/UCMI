d6f151323cbc4827767461c9f159c780d9565b04
universitariodematematica-star <universitariodematematica@gmail.com>
2026-09-15 00:31:22 -0500

    Update estado-contacto-alumno.js

--- learningbridge/estado-contacto-alumno.js
+++ learningbridge/estado-contacto-alumno.js
@@ -580,6 +580,8 @@ DETECTAR CLICS
 
 document.addEventListener("click", function(event) {
 
+document.addEventListener("click", function(event) {
+
 const boton =
     event.target.closest("button");
 
